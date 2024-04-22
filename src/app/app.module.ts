60527bd121a422f7315928fbd5fcae0358f97196
jihenJN <jihenjihen23@gmail.com>
2024-04-22 23:07:35 +0100

    feat; partners is ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ValueComponent } from './value/value.component';
 import { ServicesComponent } from './services/services.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavbarComponent, HomeComponent, AboutComponent, ValueComponent, ServicesComponent],
+  declarations: [AppComponent, FooterComponent, NavbarComponent, HomeComponent, AboutComponent, ValueComponent, ServicesComponent, PartnersComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
